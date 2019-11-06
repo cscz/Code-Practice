53ac7035154e1ef7afe4bdd1f9634217fd9f51d0
eggbiu <lhzsdu@gmail.com>
2019-11-05 21:50:41 -0600

    Number string convert

--- java/Number2String.java
+++ java/Number2String.java
@@ -8,8 +8,10 @@ public class Number2String {
 	public static void main(String args[]) {
 		for(String s : args) {
 			int cur = Integer.parseInt(s);
-			if(cur >= 0 && cur <= 26) {
-				cur = 'A' + cur;
+			//if(cur >= 0 && cur < 26) {
+			if(cur >= 1 && cur <= 26) {
+				//cur = 'A' + cur;
+				cur = 'A' + cur - 1;
 				System.out.print((char) cur);
 				//System.out.print((char) cur + " ");
 			}
--- java/String2Number.java
+++ java/String2Number.java
@@ -8,12 +8,13 @@ public class String2Number {
 	public static void main(String args[]) {
 		for(String s : args) {
 			char[] str = s.toCharArray();
+			System.out.print(s.toUpperCase() + "\t");
 			for(char c : str) {
 				if(c >= 'a' && c <= 'z') {
-					System.out.print(c - 'a' + " ");
+					System.out.print(c + 1 - 'a' + " ");
 				}
 				if(c >= 'A' && c <= 'Z') {
-					System.out.print(c - 'A' + " ");
+					System.out.print(c + 1 - 'A' + " ");
 				}
 			}
 			System.out.println(); 
